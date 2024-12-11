b066ad8f3b0cc45d4978369ce9718c23a814d803
DanCvejn <70745296+DanCvejn@users.noreply.github.com>
2024-12-11 23:46:46 +0100

    day 11 - possible part 2

--- 11/index.ts
+++ 11/index.ts
@@ -1,31 +1,46 @@
-const input11 = `125 17`;
-// const input11 = `5 62914 65 972 0 805922 6521 1639064`;
+// const input11 = `125 17`;
+const input11 = `5 62914 65 972 0 805922 6521 1639064`;
+
+const addToStones = (stones: { [key: string]: number }, stone: string) => {
+  if (!stones[stone]) {
+    stones[stone] = 1;
+  } else {
+    stones[stone] += 1;
+  }
+  return stones;
+};
+
 
 const replaceStones = (blinks: number) => {
-  let stones = input11.split(" ");
-  let sum = 0;
-  stones.forEach((stone) => {
-    let nextStones: string[] = [stone];
-    for (let i = 0; i < blinks; i++) {
-      let nextStones2: string[] = [];
-      console.log(i + 1, nextStones.length);
-
-      nextStones.forEach((stone2) => {
-        if (stone2 === "0") {
-          nextStones2.push("1");
-        } else if (stone2.length % 2 === 0) {
-          nextStones2.push(String(Number(stone2.slice(0, stone2.length / 2))));
-          nextStones2.push(String(Number(stone2.slice(stone2.length / 2))));
+  let stonesTmp = input11.split(" ");
+  let stones = {};
+  stonesTmp.forEach((stone) => {
+    stones = addToStones(stones, stone);
+  });
+  for (let i = 0; i < blinks; i++) {
+    let newStones = {};
+    console.log(`Step ${i + 1}`);
+    for (let stone in stones) {
+      for (let j = 0; j < stones[stone]; j++) {
+        if (stone === "0") {
+          newStones = addToStones(newStones, "1");
+        } else if (stone.length % 2 === 0) {
+          newStones = addToStones(newStones, String(Number(stone.slice(0, stone.length / 2))));
+          newStones = addToStones(newStones, String(Number(stone.slice(stone.length / 2))));
         } else {
-          nextStones2.push(String(Number(stone2) * 2024));
+          newStones = addToStones(newStones, String(Number(stone) * 2024));
         }
-      })
-      nextStones = nextStones2;
+      }
     }
-    sum += nextStones.length;
+    stones = newStones;
+  }
+
+  let sum = 0;
+  Object.keys(stones).forEach((stone) => {
+    sum += stones[stone];
   })
   return sum;
-}
+};
 
 
 console.log(replaceStones(25));
