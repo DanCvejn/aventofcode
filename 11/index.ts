cd6f6ea4cbc21f3466ed421d1f7031e7697bcf6b
DanCvejn <dancvejn29@gmail.com>
2024-12-12 09:11:12 +0100

    day 11 - part 2 sollution

--- 11/index.ts
+++ 11/index.ts
@@ -1,11 +1,11 @@
 // const input11 = `125 17`;
 const input11 = `5 62914 65 972 0 805922 6521 1639064`;
 
-const addToStones = (stones: { [key: string]: number }, stone: string) => {
+const addToStones = (stones: { [key: string]: number }, stone: string, toAdd: number = 1) => {
   if (!stones[stone]) {
-    stones[stone] = 1;
+    stones[stone] = toAdd;
   } else {
-    stones[stone] += 1;
+    stones[stone] += toAdd;
   }
   return stones;
 };
@@ -19,17 +19,14 @@ const replaceStones = (blinks: number) => {
   });
   for (let i = 0; i < blinks; i++) {
     let newStones = {};
-    console.log(`Step ${i + 1}`);
     for (let stone in stones) {
-      for (let j = 0; j < stones[stone]; j++) {
-        if (stone === "0") {
-          newStones = addToStones(newStones, "1");
-        } else if (stone.length % 2 === 0) {
-          newStones = addToStones(newStones, String(Number(stone.slice(0, stone.length / 2))));
-          newStones = addToStones(newStones, String(Number(stone.slice(stone.length / 2))));
-        } else {
-          newStones = addToStones(newStones, String(Number(stone) * 2024));
-        }
+      if (stone === "0") {
+        newStones = addToStones(newStones, "1", stones[stone]);
+      } else if (stone.length % 2 === 0) {
+        newStones = addToStones(newStones, String(Number(stone.slice(0, stone.length / 2))), stones[stone]);
+        newStones = addToStones(newStones, String(Number(stone.slice(stone.length / 2))), stones[stone]);
+      } else {
+        newStones = addToStones(newStones, String(Number(stone) * 2024), stones[stone]);
       }
     }
     stones = newStones;
